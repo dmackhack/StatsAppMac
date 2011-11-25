fe6abe01e22b922997509fffa467bb1a1b14aae0
dmackhack <dmackenz1981@gmail.com>
2011-11-25 22:44:13 +1100

    Initial Commit

---
+++ StatsAppMacTests/StatsAppMacTests.h
@@ -0,0 +1,17 @@
+//
+//  StatsAppMacTests.h
+//  StatsAppMacTests
+//
+//  Created by David Mackenzie on 25/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface StatsAppMacTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ StatsAppMacTests/StatsAppMacTests.m
@@ -0,0 +1,33 @@
+//
+//  StatsAppMacTests.m
+//  StatsAppMacTests
+//
+//  Created by David Mackenzie on 25/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "StatsAppMacTests.h"
+
+
+@implementation StatsAppMacTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StatsAppMacTests");
+}
+
+@end
