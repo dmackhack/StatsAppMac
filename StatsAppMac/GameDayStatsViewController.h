2e0eb61a73a18c7c5fd9b9c3f2bcbfb70e3e3eb4
dmackhack <dmackenz1981@gmail.com>
2011-11-25 22:57:58 +1100

    Initial project

---
+++ StatsAppMac/GameDayStatsViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameDayStatsViewController.h
+//  StatsAppMac
+//
+//  Created by David Mackenzie on 25/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GameDayStatsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+    
+}
+
+@end
